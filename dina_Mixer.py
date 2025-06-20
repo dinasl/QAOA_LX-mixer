4fb6ef5ade1060a26d5fbca8cb57ae3e6dcb65a1
dinasl <dinasyverudlindland@gmail.com>
2025-06-20 15:54:48 +0200

    Start of code sceleton.

--- dina_Mixer.py
+++ dina_Mixer.py
@@ -4,11 +4,10 @@ import numpy as np
 import json
 # from scipy.special import comb
 import sys
+from dataclasses import dataclass
 
 import math
 #import openquantumcomputing._rust
-
-from tqdm import tqdm
 
 from sympy import *
 from sympy.physics.paulialgebra import Pauli, evaluate_pauli_product
@@ -16,7 +15,22 @@ from sympy.physics.paulialgebra import Pauli, evaluate_pauli_product
 
 #from networkx.algorithms.approximation import clique
 
-class Mixer:
+#
+@dataclass
+class MixerGraph:
+    
+    X : int
+    G : nx.Graph
+    cost : int
+    
+    """
+    Attributes:
+        X (int): Mixer mask (integer representation).
+        G (nx.Graph): The graph representing the mixer.
+        cost (int): The cost associated with the mixer.
+    """
+
+class LXMixer:
     def __init__(self, B, digraph=False, reduced=True, sort=False, blacklist=[], whitelist=None):
         self.setB(B, sort)
         self.digraph=digraph
@@ -48,3 +62,52 @@ class Mixer:
         self.B = []
         for b in B:
             self.B.append(BitString(1,b))
+            
+    def compute_family_of_valid_mixergraphs(self):
+        """
+        Computes the family of valid mixers for the feasible set B.
+        
+        List of MixerGraph structs. 
+        
+        Avoid making duplicates (make struct in one go)? If not, set(). 
+        """
+        pass
+    
+#def main loop
+    # compute family of valid mixer graphs
+    #if choose_n: b_func
+        #n = 1
+        #connected = False
+        #while not connected:
+            #all combinations of n graphs
+            #check if any connected:
+                #append to list, connected = True
+            #n+=1
+    #compute all subgraphs MixerGraphs (with projectors?) compute_all_subgraphs()
+    #check all connected combinations with cost
+    #take lowest --> output MixerGraph with lowest cost
+    
+    #projector
+
+def compute_all_subgraphs(mixergraph_list):
+    
+    """
+    Args:
+        graph_list (list[MixerGraphs]]): List of MixerGraphs structs.
+    """
+    pass
+
+def cost():
+    pass
+
+def check_if_orbit():
+    pass
+
+def algorithm_1():
+    pass
+
+def linalg_approach():
+    pass
+
+def stabilizer_approach():
+    pass
