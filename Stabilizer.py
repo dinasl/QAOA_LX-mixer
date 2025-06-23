875b44484d2419a0b5a0c8d22df8db6d492c6acf
sannesg <Sanne.selmer.gilje@sintef.no>
2025-06-23 13:31:53 +0200

    added brute force method to check if a set of feasible states is an orbit.

--- Stabilizer.py
+++ Stabilizer.py
@@ -10,21 +10,32 @@ def check_if_orbit(B):
     Returns:
         bool: True if B is an orbit of a stabilizer group, False otherwise.
     """
-    pass
+    orbit = True
+    #This loop finds if there is an orbit when B=2^n states
+    for i in range(len(B)-1):
+        X_1 = B[i]^B[i+1] #making X_1 = z1 ^ z2
+        
+        #checks if it maps all, need a way to minimize this one so that it doesn't iterate over the same ones
+        for state in B:                     #TODO at the very least we don't need to check the ones that 
+            if (X_1 ^ state) not in B:
+                orbit = False
+                break
+    
+    return orbit
 
-def compute_minimal_generating_set():
+def compute_minimal_generating_set(B):
     """
     Computes the minimal generating set of a stabilizer group that contains the orbit B.
     
     Args:
-        B (list[int]): Feasible set of bitstrings (int representations) from the computational basis.
+        B (list[int]): Feasible set of bitstrings (int representations) from the computational basis that is an orbit.
     
     Returns:
         list[int]: List of Pauli strings (int representation) that form the minimal generating set.
     """
     pass
 
-def compute_restricted_projector_linalg(stabilizer_group, B):
+#def compute_restricted_projector_linalg(stabilizer_group, B):
     """
     Computes the restricted projector using linear algebra approach.
     
@@ -35,14 +46,13 @@ def compute_restricted_projector_linalg(stabilizer_group, B):
     Returns:
         ??? : The restricted projector in the form of a (???, vector) or other suitable representation.
     """
-    pass
 
-def compute_restricted_projector_stabilizer():
+def compute_restricted_projector_stabilizer(minimal_generating_set, B):
     """
     Computes the restricted projector using the stabilizer formalism approach.
     
     Args:
-        stabilizer_group (list[int]): List of Pauli strings (int representation) that form the stabilizer group.
+        minimal generating set of stabilizer_group (list[int]): List of Pauli strings (int representation) that form the stabilizer group.
         B (list[int]): Feasible set of bitstrings (int representations) from the computational basis.  
     
     Returns:
