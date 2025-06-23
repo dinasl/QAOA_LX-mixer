2317450903748c18fbb3c8f23e018903cce2ca31
sannesg <Sanne.selmer.gilje@sintef.no>
2025-06-23 15:10:23 +0200

    added a more effiecent check_if_orbit, and put brute force method in comments. Also added find_orbit_X_string

--- Stabilizer.py
+++ Stabilizer.py
@@ -10,18 +10,37 @@ def check_if_orbit(B):
     Returns:
         bool: True if B is an orbit of a stabilizer group, False otherwise.
     """
-    orbit = True
-    #This loop finds if there is an orbit when B=2^n states
+    #This loop finds if there is an orbit when B=2^n states, iterates over all the possible X's and states
+    B_set = set(B)
+    tried_X = set()
+
+    for i in range(len(B)-1):
+        X_1 = B[i]^B[i+1] #making X_1 = z1 ^ z2
+        if X_1 in tried_X:
+            continue
+        tried_X.add(X_1)
+        
+        #checks if it maps all states to another
+        if not all((X_1 ^ state) in B_set for state in B):
+            return False
+    
+    return True
+    #could also make a graph with nodes B, and add edges with X's...(?)
+    """
+    #brute force:
     for i in range(len(B)-1):
         X_1 = B[i]^B[i+1] #making X_1 = z1 ^ z2
         
         #checks if it maps all, need a way to minimize this one so that it doesn't iterate over the same ones
         for state in B:                     #TODO at the very least we don't need to check the ones that 
             if (X_1 ^ state) not in B:
-                orbit = False
-                break
+                return False
     
-    return orbit
+    return True
+    """
+
+def find_orbit_X_string(B):
+    pass
 
 def compute_minimal_generating_set(B):
     """
@@ -59,3 +78,6 @@ def compute_restricted_projector_stabilizer(minimal_generating_set, B):
         ??? : The restricted projector in the form of a (???, vector) or other suitable representation.
     """
     pass
+
+B = [0b1011, 0b1100, 0b0111, 0b0000, 0b1110, 0b1001, 0b0010]
+print(check_if_orbit(B))
