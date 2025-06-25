a555c88fafe3b202d2f1b858235c7dc6b5be3530
sannesg <Sanne.selmer.gilje@sintef.no>
2025-06-25 08:48:42 +0200

    checking out check_if_orbit

--- Stabilizer.py
+++ Stabilizer.py
@@ -13,9 +13,9 @@ def check_if_orbit(B):
     """
     #TODO not completely sure it includes absolutely all cases...
     #This loop finds if there is an orbit when B=2^n states, iterates over all the possible X's and states
+    
     B_set = set(B)
     tried_X = set()
-
     for i in range(len(B)-1):
         X_1 = B[i]^B[i+1] #making X_1 = z1 ^ z2
         if X_1 in tried_X:
@@ -56,12 +56,9 @@ def compute_minimal_generating_set(B, n):
     if check_if_orbit(B)[0]:
         orbit = check_if_orbit(B)[1]
     
-    #use seed B[0] to get G0 which is on the form G0 = {(+-1, ZII...), ...} where Z is represented by 1 and I by 0
+    #use seed B[0] to get G0 which is on the form G0 = {(+-1, ZII...), ...} where the z-string is on binary (int) form and Z is represented by 1 and I by 0
     G0 = [((-1 if (B[0] >> (n - 1 - i)) & 1 else 1), 1 << (n - 1 - i)) for i in range(n)]
 
-    
-    
-    
     #iteration process for algoritm 1
     for x_string in orbit:
         G0_elements = [t[1] for t in G0]    #selects all of the elements of G that is a z-string (without +-1)
@@ -79,18 +76,18 @@ def compute_minimal_generating_set(B, n):
             else:
                 I_d.append(index) #appends the position of the anti-commuting string
 
-        #creates the anti-commuting pairs
+        #creates the anti-commuting pairs, it is now a list withing a list: ex: [[2, 3], [2, 4], ...]
         I_d_2 = list(itertools.combinations(I_d, 2)) 
         
         #only iterating over necessary pairs and stores the relevant ones
         elements_included = len(G0_elements) - len(I_c) - 1
         I_d_2_shortened_Z = []
 
-        if len(I_d_2) > 0: #checking that we have anti-commuting pairs
+        if len(I_d_2) > 0: #checking that we have anti-commuting pairs #TODO I am not sure this one is necessary anymore...
             for i in range(elements_included): 
                 #creates a tuple with (+-1, ZiZj) for anti-commuting pairs 
                 #+-1 is the signs multiplied and ZiZj is the bitstrings combined
-                I_d_2_shortened_Z.append(( G0_signs[I_d_2[i][0]]*G0_signs[I_d_2[i][1]],G0_elements[I_d_2[i][0]]|G0_elements[I_d_2[i][1]]))
+                I_d_2_shortened_Z.append((G0_signs[I_d_2[i][0]]*G0_signs[I_d_2[i][1]],G0_elements[I_d_2[i][0]]|G0_elements[I_d_2[i][1]]))
 
         #creates a list of tuples (+-1, Z-string) for commuting pairs  
         I_c_Z = [(G0_signs[i], G0_elements[i]) for i in I_c]
@@ -113,11 +110,16 @@ def compute_restricted_projector_stabilizer(B, n):
         ??? : The restricted projector in the form of a (???, vector) or other suitable representation.
     """
     minimal_generating_set = compute_minimal_generating_set(B, n)
-    matrix = np.zeros((len(B), len(minimal_generating_set)))
+    matrix = np.zeros((len(B), len(minimal_generating_set[:][1])))
     #finding elements for matrix
     for i in B:
-        matrix_row = [i & stabilizer for stabilizer in minimal_generating_set]
-        #using Brian Kernighans's algorithm to check parity #TODO ¨
+        matrix_row_binary = [i & stabilizer for stabilizer in minimal_generating_set] 
+        matrix_row = []                                                                          #TODO update this to numpy array so that it is faster
+        for index, string in enumerate(matrix_row_binary):
+            parity_string = parity(string)
+            sign = parity_string*minimal_generating_set[index][0]
+            
+            matrix_row.append(sign)
 
         matrix[i] = matrix_row
     print(matrix)
@@ -131,9 +133,10 @@ def parity(n):
     return parity
 
 B = [0b1011, 0b1100, 0b0111, 0b0000, 0b1110, 0b1001, 0b0010, 0b0101]
-print(compute_minimal_generating_set(B, 4))
+compute_minimal_generating_set(B, 4)
 B1 = [0b11101, 0b01010, 0b10011, 0b00110]
 G = [(-1, 0b00010), (-1, 0b00001), (-1, 0b11000), (1, 0b01100)]
+compute_restricted_projector_stabilizer(B1, 5)
 
 
 
