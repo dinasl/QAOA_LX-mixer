f8f7c15b51fb1532bd988382f5b1715c03fdcd36
sannesg <Sanne.selmer.gilje@sintef.no>
2025-07-08 13:07:28 +0200

    added function convert_to_binary_strings

--- utils.py
+++ utils.py
@@ -35,3 +35,35 @@ def parity(n):
     else:
         parity = -1
     return parity
+
+def convert_to_binary_string(int_values, n):
+    """
+    Convert integers to binary strings with n bits.
+    
+    - If int_values is a single int: return binary string
+    - If it's a list: return list of binary strings or (bin, bin) tuples
+    - If it's a dict: return dict with binary string keys and values
+    """
+    
+    if isinstance(int_values, int):
+        return format(int_values, f'0{n}b')
+
+    elif isinstance(int_values, list):
+        result = []
+        for i in int_values:
+            if isinstance(i, int):
+                result.append(format(i, f'0{n}b'))
+            elif isinstance(i, tuple) and len(i) == 2:
+                if all(isinstance(x, int) for x in i):
+                    result.append((format(i[0], f'0{n}b'), format(i[1], f'0{n}b')))
+        return result
+
+    elif isinstance(int_values, dict):
+        result = {}
+        for key, val in int_values.items():
+            if isinstance(key, int) and isinstance(val, int):
+                result[format(key, f'0{n}b')] = format(val, f'0{n}b')
+        return result
+
+    else:
+        raise TypeError("Input must be int, list, or dict of ints")
