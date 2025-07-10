269f2a5a8ffdcc915cbd9ebd6fadd315626484ae
sannesg <Sanne.selmer.gilje@sintef.no>
2025-07-10 16:29:32 +0200

    updated test_stabilizer_class.py (unittest) for test_compute_minimal_generating_sets. Does not work for test_compute_projector_stabilizers yet

--- unittests/test_stabilizer_class.py
+++ unittests/test_stabilizer_class.py
@@ -47,55 +47,69 @@ import sys
 import os
 import copy
 
+
 # Add path to module
 sys.path.insert(0, os.path.abspath("C:/Users/sanne/QAOA_LX-mixer"))
 from Stabilizer import *
+from Mixer import Orbit
 
 class TestStabilizer(unittest.TestCase):
     def setUp(self):
         self.test_cases = [
             {
-                "B": [[0b1011, 0b1100, 0b0111, 0b0000, 0b1110, 0b1001, 0b0010, 0b0101]],
+                "B": [0b1011, 0b1100, 0b0111, 0b0000, 0b1110, 0b1001, 0b0010, 0b0101],
                 "n": 4,
-                "expected_orbits": [[11, 14, 7]],
-                "expected_mgs": [[(1, 13)]],
-                "expected_projectors": [[(1, 0), (1, 13)]]
+                "orbit_dictionary": {(0,1,2,3,4,5,6,7): Orbit(Xs={11, 14, 7})},
+                "expected_orbits": {(0,1,2,3,4,5,6,7) : [11, 14, 7]},
+                "expected_mgs": {(0,1,2,3,4,5,6,7):[(1, 13)]},
+                "expected_projectors": [(1, 0), (1, 13)]
             },
             {
-                "B": [[0b11000, 0b00100, 0b01101, 0b10001]],
+                "B": [0b11000, 0b00100, 0b01101, 0b10001],
                 "n": 5,
-                "expected_orbits": [[9, 28]],
-                "expected_mgs": [[(1, 2), (-1, 20), (1, 25)]],
-                "expected_projectors": [[(1, 0), (1, 25), (-1, 20), (-1, 13), (1, 2), (1, 27), (-1, 22), (-1, 15)]]
+                "orbit_dictionary": {(0,1,2,3): Orbit(Xs={9, 28})},
+                "expected_orbits": {(0,1,2,3): [9, 28]},
+                "expected_mgs": {(0,1,2,3) : [(1, 2), (-1, 20), (1, 25)]},
+                "expected_projectors": [(1, 0), (1, 25), (-1, 20), (-1, 13), (1, 2), (1, 27), (-1, 22), (-1, 15)]
             }
         ]
-
-    def test_check_if_orbit(self):
-        for case in self.test_cases:
-            with self.subTest(case=case):
-                case = copy.deepcopy(case)
-                stab = Stabilizer(familiy_of_graphs=None, B=case["B"], n=case["n"])
-                stab.check_if_orbit()
-                self.assertEqual(stab.orbits, case["expected_orbits"])
 
     def test_compute_minimal_generating_sets(self):
         for case in self.test_cases:
             with self.subTest(case=case):
                 case = copy.deepcopy(case)
-                stab = Stabilizer(familiy_of_graphs=None, B=case["B"], n=case["n"])
-                stab.orbits = case["expected_orbits"]
+                stab = Stabilizer(B=case["B"], n=case["n"], orbit_dictionary=case["orbit_dictionary"])
                 stab.compute_minimal_generating_sets()
-                self.assertEqual(stab.minimal_generating_sets, case["expected_mgs"])
-
+                for orbit_key, orbit in stab.orbit_dictionary.items():
+                    with self.subTest(orbit_key=orbit_key):
+                        self.assertEqual(orbit.Zs, case["expected_mgs"][orbit_key])
+    
+    #TODO need to fix this one
     def test_compute_projector_stabilizers(self):
         for case in self.test_cases:
             with self.subTest(case=case):
                 case = copy.deepcopy(case)
-                stab = Stabilizer(familiy_of_graphs=None, B=case["B"], n=case["n"])
+                stab = Stabilizer(B=case["B"], n=case["n"], orbit_dictionary=case["orbit_dictionary"])
+                stab.minimal_generating_sets = case["expected_mgs"]
+                stab.compute_projector_stabilizers()
+
+                # Ensure same number of projectors
+                self.assertEqual(len(stab.projectors), len(case["expected_projectors"]))
+
+                # Compare each projector individually for more informative test output
+                for computed, expected in zip(stab.projectors, case["expected_projectors"]):
+                    with self.subTest(projector=(computed, expected)):
+                        self.assertEqual(computed, expected)
+    """
+    def test_compute_projector_stabilizers(self):
+        for case in self.test_cases:
+            with self.subTest(case=case):
+                case = copy.deepcopy(case)
+                stab = Stabilizer(B=case["B"], n=case["n"])
                 stab.orbits = case["expected_orbits"]
                 stab.minimal_generating_sets = case["expected_mgs"]
                 stab.compute_projector_stabilizers()
                 self.assertEqual(stab.projectors, case["expected_projectors"])
-
+    """
 if __name__ == '__main__':
     unittest.main()
