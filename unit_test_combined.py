d3232b5d69e600269a30ece9d97345d24bbdeded
sannesg <Sanne.selmer.gilje@sintef.no>
2025-07-14 14:30:17 +0200

    added new example to unit_test_combined.py

--- unit_test_combined.py
+++ unit_test_combined.py
@@ -2,8 +2,8 @@
 from Stabilizer import Stabilizer
 from Mixer import LXMixer
 
-B = [0b1110, 0b1100, 0b1001, 0b0100, 0b0011]
-
+#B = [0b1110, 0b1100, 0b1001, 0b0100, 0b0011]
+B = [0b1110, 0b1100, 0b1001, 0b0100, 0b0011, 0b0000, 0b1111, 0b1011, 0b1101, 0b0110, 0b0010, 0b0101, 0b1000, 0b0001, 0b0111]
 lxmixer = LXMixer(B, 4)
 lxmixer.compute_family_of_valid_graphs()
 lxmixer.compute_all_orbits()
