a1195ac0b3a5ab77f5337f942addebdfcb971b74
dinasl <dinasyverudlindland@gmail.com>
2025-08-05 14:08:36 +0200

    Added a brute-force method (in the greatest_orbit_heuristic parameter) to compare the greatest-orbit heuristic to the brute-force solution. Works and runs relatively quick.

--- Mixer.py
+++ Mixer.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Tuple, Set
 from dataclasses import dataclass, field
 from itertools import combinations
 import math
+from tqdm import tqdm
 
 from Stabilizer import *
 from utils import is_connected, is_power_of_two, find_best_cost
@@ -61,7 +62,7 @@ class LXMixer:
         find_best_mixer(): Finds the best mixer based on the computed orbits, edges, minimal generating sets, projectors and costs.
 
     """
-    def __init__(self, B, nL, digraph=False, reduced=True, sort=False, blacklist=[], whitelist=None):
+    def __init__(self, B, nL, digraph=False, reduced=True, sort=False, greatest_orbit_heuristic=True, blacklist=[], whitelist=None):
         """
         Initializes the LXMixer with the feasible set B and number of logical qubits nL.
 
@@ -78,6 +79,8 @@ class LXMixer:
         
         self.digraph=digraph
         self.reduced=reduced
+        
+        self.greatest_orbit_heuristic = greatest_orbit_heuristic
         
         # Prepare empty attributes
         self.family_of_valid_graphs : Dict[int, List[Tuple[int,...]]] = {} # Maps logical X operators to edges (tuples of node indices) connected by the operator X_ij : (i,j).
@@ -145,7 +148,7 @@ class LXMixer:
         processed_nodes = set()
         
         for seed in range(self.nB): # Iterate over each node in B as a seed node.
-            if seed in processed_nodes: # Skip if the seed node is already present in a non-trivial orbit.
+            if seed in processed_nodes and self.greatest_orbit_heuristic: # Skip if the seed node is already present in a non-trivial orbit.
                 continue
             
             seed_Xs = list(self.node_connectors[seed].values()) # The |B|-1 logical X operators that connect to the seed node.
@@ -159,15 +162,19 @@ class LXMixer:
                 available_Xs: List of logical X operators that can be added to the current path, i.e. those that have not been used yet.
                 current_nodes: Tuple of nodes that are connected by the current path.
                 """
-                current_nodes_tuple = tuple(sorted(current_nodes)) # Sort the nodes for consistent key representation.
-                if not self.orbits.keys(): # If `orbits` is empty, add the first orbit to it.
-                    self.orbits[current_nodes_tuple] = Orbit(Xs=current_path)
-                elif not any(set(current_nodes).issubset(set(nodes)) for nodes in list(self.orbits.keys())): # Else, if the current nodes are not a subset of any existing orbit, add a new orbit.
-                    self.orbits[current_nodes_tuple] = Orbit(Xs=current_path)
-                    for nodes in list(self.orbits.keys()): # Remove orbits that are subsets of the current nodes that were just added.
-                        if set(nodes) < set(current_nodes):
-                            self.orbits.pop(nodes)
-                    if len(current_nodes) > 2: processed_nodes.update(current_nodes) # Mark nodes in non-trivial orbits as processed. Will be skipped when these seeds are reached.
+                if len(current_nodes) > 1:
+                    current_nodes_tuple = tuple(sorted(current_nodes)) # Sort the nodes for consistent key representation.
+                    if not self.orbits.keys(): # If `orbits` is empty, add the first orbit to it.
+                        self.orbits[current_nodes_tuple] = Orbit(Xs=current_path)
+                    elif not any(set(current_nodes).issubset(set(nodes)) for nodes in list(self.orbits.keys())) or not self.greatest_orbit_heuristic: # Else, if the current nodes are not a subset of any existing orbit, add a new orbit.
+                        self.orbits[current_nodes_tuple] = Orbit(Xs=current_path)
+                        
+                        if self.greatest_orbit_heuristic:
+                            for nodes in list(self.orbits.keys()): # Remove orbits that are subsets of the current nodes that were just added.
+                                if set(nodes) < set(current_nodes):
+                                    self.orbits.pop(nodes)
+                                    
+                        if len(current_nodes) > 2: processed_nodes.update(current_nodes) # Mark nodes in non-trivial orbits as processed. Will be skipped when these seeds are reached.
                 
                 for x, X in enumerate(available_Xs): # Iterate over the available X operators to choose from at this point in the tree. 
                     new_path = current_path + [X] # Add the new X operator to the path.
@@ -197,18 +204,19 @@ class LXMixer:
         Finds the best mixer and updates the `best_Xs`, `best_Zs`, `best_combinations` and `best_cost` attributes.
         """ 
         # If all the nodes in B are connected by a single orbit, this is the best mixer.
-        if len(self.orbits.keys()) == 1:
-            self.best_Xs = [[list(self.orbits.values())[0].Xs]]
-            self.best_Zs = [[list(self.orbits.values())[0].Zs]]
-            self.best_cost = list(self.orbits.values())[0].cost
-            self.best_combinations = [tuple(self.orbits.keys())]
+        B_orbit = tuple(range(self.nB)) # All node indexes in B.
+        if B_orbit in self.orbits.keys():
+            self.best_Xs = [[self.orbits[B_orbit].Xs]]
+            self.best_Zs = [[self.orbits[B_orbit].Zs]]
+            self.best_cost = self.orbits[B_orbit].cost
+            self.best_combinations = [[B_orbit]]
             return
         
         # If there are multiple orbits, we must at least take a combination of two orbits to form a connected mixer.
-        N = range(2, len(self.orbits.keys())+1) # Range of the number of orbits to combine. Goes from 2 to the number of orbits.
+        N = range(2, self.nB) # Range of the number of orbits to combine. Goes from 2 to |B|-1.
         for n in N:
-            for combination in combinations(self.orbits.keys(), n): # TODO: tqdm progress bar?
-                if len(set([node for nodes in combination for node in nodes])) != self.nB: # If the combination does not cover all nodes in B, skip it.
+            for combination in tqdm(combinations(self.orbits.keys(), n), desc=f"n = {n}"): # TODO: Makes duplicates?
+                if len({node for nodes in combination for node in nodes}) != self.nB: # If the combination does not cover all nodes in B, skip it.
                     continue
                 if not is_connected(combination): # If the combination does not connect all nodes, skip it.
                     continue
@@ -233,16 +241,18 @@ if __name__ == '__main__':
     import time # For measuring execution time.
     
     # nL = 3
+    # B = [0, 1, 2, 3, 4, 5, 6, 7]
     # B = [6, 3, 1, 5, 0, 4, 2] # nB = 7
     # B = [6, 2, 1, 0, 5] # nB = 5
     # B = [6,5] # nB = 2
     
     # nL = 4
     # B = [0b1110, 0b1100, 0b1001, 0b0100, 0b0011] # nB = 5, example from the article
+    # B = [0b0000, 0b1111, 0b0001, 0b1101, 0b1110, 0b1100] # nB = 6
+    B = [0b0000, 0b1111, 0b0001, 0b1101, 0b1110, 0b1100, 0b0010] # nB = 7
     # B = [0b0000, 0b1111, 0b0001, 0b1101, 0b1110, 0b1100, 0b0010, 0b0011] # nB = 8, 8-orbit
-    # B = [0b0000, 0b1111, 0b0001, 0b1101, 0b1110, 0b1100, 0b0010] # nB = 7
-    B = [0b1110, 0b1100, 0b1001, 0b0100, 0b0011, 0b0000, 0b1111, 0b1011, 
-         0b1101, 0b0110, 0b0010, 0b0101, 0b1000, 0b0001, 0b0111] # nB = 15
+    # B = [0b1110, 0b1100, 0b1001, 0b0100, 0b0011, 0b0000, 0b1111, 0b1011, 
+    #      0b1101, 0b0110, 0b0010, 0b0101, 0b1000, 0b0001, 0b0111] # nB = 15
     
     # nL = 5
     # B = [0b00001, 0b00010, 0b00100,
@@ -251,14 +261,14 @@ if __name__ == '__main__':
     #     0b01010, 0b01100, 0b10001, 
     #     0b10010, 0b10100, 0b11000] # nB = 15
     # B = [0b10011, 0b01100, 0b11000, 0b00011,
-    #     0b01001, 0b10100, 0b00110, 0b01110] nB = 8
+    #     0b01001, 0b10100, 0b00110, 0b01110] # nB = 8
     
     print(f"\nB = {[f"{b:0{len(bin(max(B)))-2}b}" for b in B]}") # Print B in binary format.
     
     # Initialize the LXMixer with the feasible set B and number of logical qubits nL.
     # lxmixer = LXMixer(B, 3)
-    lxmixer = LXMixer(B, 4)
-    # lxmixer = LXMixer(B, 5)
+    lxmixer = LXMixer(B, 4, greatest_orbit_heuristic=False)
+    # lxmixer = LXMixer(B, 5, greatest_orbit_heuristic=False)
 
     print("\nComputing family of valid graphs...")
     start_time = time.time()
