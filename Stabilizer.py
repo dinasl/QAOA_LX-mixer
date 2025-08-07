8b6dc6303d0066f5f840824a36506ac6f3e3a0b8
sannesg <Sanne.selmer.gilje@sintef.no>
2025-08-07 13:33:47 +0200

    deleted unnecessary code/unused code and cleaned up the comments

--- Stabilizer.py
+++ Stabilizer.py
@@ -3,7 +3,6 @@ import itertools
 import utils
 from functools import reduce
 from sympy import Matrix, GF
-import math
 
 class Stabilizer:
     def __init__(self, B, n, orbit_dictionary):
@@ -14,61 +13,20 @@ class Stabilizer:
             family_of_graphs (...):
             B ([[]]): Feasible set of bitstrings (int representations) from the computational basis that is an orbit.
             n (int): number of qubits
-            orbits ([[ ]]):
-            valid_states ([[[,]]]): The states that is defined in the original problem as valid. Does not need to be an orbit
-            minimal_generating_set ([[()]]):
-            projector ([[[,]]]):
+            orbit_dictionary (Dict{sort_tuple: Class}): Dictionary with nodes as keys (represented in tuples) and the class Orbit as values (containing lists of values)
 
         """
         self.B = B 
         self.n = n
         self.orbit_dictionary = orbit_dictionary
 
-    
-    #helping function to test the code before adding the find_orbit_function
-    def print_values(self):
-        return "orbits: " + str(self.orbits) + "\nMin gen set: " + str(self.minimal_generating_sets) + "\nProjectors: " + str(self.projectors)
-
-    def check_if_orbit(self):
-        """
-        Checks if the set B is an orbit of a stabilizer group.
-
-        Args:
-            B (list[int]): Feasible set of bitstrings (int representations) from the computational basis. 
-            
-        Returns:
-            bool: True if B is an orbit of a stabilizer group, False otherwise.
-        """
-        #This loop finds if there is an orbit when B=2^n states, iterates over all the possible X's and states, brute force
-        #not going to be necessary method
-        for B in self.B:
-            B_set = set(B)
-            tried_X = set()
-            for i in range(len(B)-1):
-                X_1 = B[i]^B[i+1] #making X_1 = z1 ^ z2
-                if X_1 in tried_X:
-                    continue
-                tried_X.add(X_1)
-                    
-                #checks if it maps all states to another
-                if not all((X_1 ^ state) in B_set for state in B):
-                    self.orbits.append([None])
-                    break
-            self.orbits.append(list(tried_X))
-    
     def compute_minimal_generating_sets(self):
         """
-        Computes the minimal generating set of a stabilizer group that contains the orbit B.
-        
-        Args:
-            orbits_new (Dict{sort_tuple: Class}): Dictionary with nodes as keys (represented in tuples) and the class Orbit as values (containing lists of values)
-            B (List[int]): 
-        
-        Returns:
-            list[int]: List of Pauli strings (int representation) that form the minimal generating set.
+        Computes the minimal generating set of a stabilizer group that contains the orbit B and updates the orbit dataclass instance with the minimal generating set for Zs.
         """
         for nodes, orbit in self.orbit_dictionary.items():
-            #found the seed from the 0th element of the tuple, which corresponds to a state saved as a bin int in self.B 
+
+            #find the seed from the 0th element of the tuple, which corresponds to a state saved as a bin int in self.B
             seed = self.B[nodes[0]]
             
             #use seed to get G0 which is on the form G0 = {(+-1, ZII...), ...} where the z-string is on binary (int) form and Z is represented by 1 and I by 0
@@ -82,6 +40,7 @@ class Stabilizer:
                 #is a string that checks if X and Z work on the same qubit for a x-string with all z-strings. Ex: 0100 means X and Z both work on qubit 2 
                 commutation_string = [x_string & z_string for z_string in G0_elements]
 
+                #I_c and I_d are lists that will contain the indices of the commuting and anti-commuting stabilizers respectively
                 I_c = []
                 I_d = []
                 for index, j in enumerate(commutation_string):      #iterates over the elements (binary strings)
@@ -98,139 +57,63 @@ class Stabilizer:
                     I_d_2 = list(itertools.islice(itertools.combinations(I_d, 2), elements_included))
                     I_d_2_Z = [(G0_signs[I_d_2[i][0]]*G0_signs[I_d_2[i][1]],G0_elements[I_d_2[i][0]]^G0_elements[I_d_2[i][1]]) for i in range(elements_included)]
                 else:
-                    #TODO what happens if there is only 1 anti-commuting stabilizer? -> just add it to the list of commuting stabilizers?
+                    # If there are 0 or 1 anti-commuting stabilizers, we just take the commuting ones
                     I_d_2_Z = [] 
                 
                 #creates a list of tuples (+-1, Z-string) for commuting pairs  
                 I_c_Z = [(G0_signs[i], G0_elements[i]) for i in I_c]
-
+                
+                #the new minimal generating set is the combination of the commuting and anti-commuting stabilizers
                 G_new = I_c_Z + I_d_2_Z
-                if len(G_new) != (len(G0)-1):
-                    print("WARNING: G_new is not the same length as G0 - 1, this should not happen")
-                    print("G_new: ", G_new)
-                    print("G0: ", G0)
+                
+                #updates G0 to the new minimal generating set
                 G0 = G_new
-            #finds the final minimal generating set and adds it to the list of minimal generating sets
-            final_minimal_generating_set_1_orbit = G0 #removed list from list(G0) since it is already a list of tuples
             
+            #finds the final minimal generating set and adds it to the list of minimal generating sets
+            final_minimal_generating_set_1_orbit = G0 
+            
+            #updates the orbit dataclass instance with the final minimal generating set
             self.orbit_dictionary[nodes].Zs = final_minimal_generating_set_1_orbit
 
     def compute_projector_stabilizers(self, restricted = False):
         """
-        Computes the restricted projectors using the stabilizer formalism approach.
-        
-        Args:
-            minimal generating set of stabilizer_group (list(tuples[int])): List of Pauli strings (int representation) that form the stabilizer group.
-            B (list[int]): Feasible set of bitstrings (int representations) from the computational basis.  
-        
-        Returns:
-            ??? : The restricted projector in the form of a (???, vector) or other suitable representation.
+        Computes the restricted projectors using the stabilizer formalism approach and updates the orbit dataclass instance with the projectors for Zs. 
         """
-        #TODO remember to normalize the projector
-        if restricted:
-            for j in range(self.minimal_generating_sets):
-                #dimension and number of rows and columns of the matrix
-                B_not_stabilized = [x for x in self.valid_states[j] if x not in self.B[j]] #TODO denne blir feil siden B bare er nodene i orbiten, må ha et overordnet set og skrive self.overordnet_set - self.B
-                minimal_generating_set = self.minimal_generating_sets[j]
+        for orbit in self.orbit_dictionary.values():
                 
-                #TODO flip the rows and columns -> better later?
-                matrix = np.zeros((len(B_not_stabilized), len(minimal_generating_set)))
-
-                #TODO make directly with 0s and 1s? switch parity back in utils, and do a True/False on minimal_generating_set[index][0]
-                for i in B_not_stabilized:
-                    #& operator on z-string (stabilizer) and state -> 1s if Z works on 1 and take parity of that
-                    #-> -1 if anticommuting and 1 if commuting -> multiply by sign of the stabilizer
-                    matrix_row = [utils.parity(i & stabilizer)*minimal_generating_set[index][0] for index, stabilizer in minimal_generating_set[:][1]]     #TODO check that minimal_generating_set[:][1] gives z-string
-                    matrix[i] = matrix_row
+            minimal_generating_set = orbit.Zs
                 
-                #convert into matrix of 0s and 1s where 1 -> 0, -1 -> 1
-                binary_matrix = (matrix == -1).astype(int)
+            #all possible combinations
+            k = len(minimal_generating_set)
+            projector = []
 
-                #transposes matrix to sove it more easily
-                matrix_transposed = Matrix(binary_matrix.T.tolist())
-
-                #creating the vector we want to find (all -1s in the article -> 1s in the matrix)
-                target = Matrix([1] * matrix.shape[0])
-
-                #finding the solution
-                sol = matrix_transposed.solve_least_squares(target, domain=GF(2))
-                
-                if sol is not None:
-                    indices = [i for i, val in enumerate(sol) if val == 1] # subset of column indices
-
-        else: #TODO ignoring global phase?
-            for orbit in self.orbit_dictionary.values():
-                
-                minimal_generating_set = orbit.Zs
-                
-                #all possible combinations
-                k = len(minimal_generating_set)
-                projector = []
-
-                #if we have an edge case where there are no minimal generating sets because |B| = 2^n, then we just return the identity projector
-                if k == 0:
-                    orbit.Zs = [(1,0)]  # Identity projector
-                    return
-               
-                signs, z_strings = zip(*minimal_generating_set)
-                signs = np.array(signs)
-                z_strings = np.array(z_strings)
+            #if |B| = 2^n we have an edge case where there are no minimal generating sets and we don't need a projector as B is the entire space. We therefore automatically return the identity projector
+            if k == 0:
+                orbit.Zs = [(1,0)]  
+                return
+           
+            signs, z_strings = zip(*minimal_generating_set)
+            signs = np.array(signs)
+            z_strings = np.array(z_strings)
             
-                # Get all binary combinations (2^k × k)
-                all_choices = np.array(list(itertools.product([0, 1], repeat=k)))  # shape (2^k, k)
+            # Get all binary combinations (2^k × k)
+            all_choices = np.array(list(itertools.product([0, 1], repeat=k)))  # shape (2^k, k)
 
-                for choice in all_choices:
-                    # Combine signs of selected generators
-                    choice = np.asarray(choice, dtype=bool)
-                    selected_signs = signs[choice]
-                    total_sign = np.prod(selected_signs) if len(selected_signs) > 0 else 1
+            for choice in all_choices:
+                # Combine signs of selected generators
+                choice = np.asarray(choice, dtype=bool)
+                selected_signs = signs[choice]
+                total_sign = np.prod(selected_signs) if len(selected_signs) > 0 else 1
                 
-                    # Combine Pauli strings using XOR
-                    selected_zs = z_strings[choice]
-                    if len(selected_zs) == 0:
-                        combined_z = 0  # identity
-                    else:
-                        # Reduce applies the function ^ iteratively, and 0 is the inital value
-                        combined_z = reduce(lambda a, b: a ^ b, selected_zs, 0)
+                # Combine Pauli strings using XOR
+                selected_zs = z_strings[choice]
+                if len(selected_zs) == 0:
+                    combined_z = 0  # identity
+                else:
+                    # Reduce applies the function ^ iteratively, and 0 is the inital value
+                    combined_z = reduce(lambda a, b: a ^ b, selected_zs, 0)
 
-                    projector.append((int(total_sign), int(combined_z)))
-            
-    
-                # Updating so that we disregard the minimal generating sets and only keep the projectors 
-                orbit.Zs = projector #changed to projector from projectors
+                projector.append((int(total_sign), int(combined_z)))
 
-                # print("Projector for a given orbit: ", orbit.Zs)
-"""
-B = [0b1011, 0b1100, 0b0111, 0b0000, 0b1110, 0b1001, 0b0010, 0b0101]
-orbit_dictionary = {"hei":"hei"}
-#compute_minimal_generating_set(B, 4)
-B1 = [0b11101, 0b01010, 0b10011, 0b00110]
-G = [(-1, 0b00010), (-1, 0b00001), (-1, 0b11000), (1, 0b01100)]
-#compute_restricted_projector_stabilizer(B1, 5)
-
-stabilizer = Stabilizer(B=B, n=4, orbit_dictionary={})
-#stabilizer.check_if_orbit()
-stabilizer.compute_minimal_generating_sets()
-#stabilizer.compute_projector_stabilizers()
-
-stabilizer2 = Stabilizer(B=[[0b11000, 0b00100, 0b01101, 0b10001]], n=5)
-stabilizer2.check_if_orbit()
-stabilizer2.compute_minimal_generating_sets()
-stabilizer2.compute_projector_stabilizers()
-print(stabilizer2.print_values())
-"""
-
-
-
-    
-#def compute_restricted_projector_linalg(stabilizer_group, B):
-"""
-    Computes the restricted projector using linear algebra approach.
-    
-    Args:
-        stabilizer_group (list[int]): List of Pauli strings (int representation) that form the stabilizer group.
-        B (list[int]): Feasible set of bitstrings (int representations) from the computational basis.
-    
-    Returns:
-        ??? : The restricted projector in the form of a (???, vector) or other suitable representation.
-"""
+            # Updating the orbit dataclass instance so that we disregard the minimal generating sets and only keep the projectors for Zs
+            orbit.Zs = projector 
